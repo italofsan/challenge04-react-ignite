c8755b2ed1085f79288a8fd16bd4f02ae5c0e870
Italo Santos <italofsan@outlook.com>
2021-07-15 02:39:50 -0300

    Conversão do arquivo index.js e styles.js do componente Header para index.tsx e styles.ts

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import Header from "../../components/Header";
+import { Header } from "../../components/Header";
 import api from "../../services/api";
 import Food from "../../components/Food";
 import ModalAddFood from "../../components/ModalAddFood";
